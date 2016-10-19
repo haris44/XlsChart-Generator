588f9c3416f09811c365d1004a160390073f4f0c
haris44 <Haris44350>
2016-10-19 14:09:21 +0200

    format table

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,8 +9,9 @@ public class Main {
 		HashMap<String, Double[]> dataMap = new HashMap<String, Double[]>();
 
 		dataMap.put("Tokyo", new java.lang.Double[]{7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9});
-		dataMap.put("NewYork",  new java.lang.Double[]{-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6});
-		dataMap.put("Berlin", new java.lang.Double[]{-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9});
+		dataMap.put("NewYork",  new java.lang.Double[]{0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6});
+		dataMap.put("Berlin", new java.lang.Double[]{0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9});
+		dataMap.put("Paris", new java.lang.Double[]{0.2, 1.6, 4.5, 8.4, 4.5, 8.0, 10.6, 19.9, 24.3, 7.0, 3.9});
 
 		new XlsGenerator("export/workbook.xls", columnsData, dataMap);
 
--- src/main/java/XlsGenerator.java
+++ src/main/java/XlsGenerator.java
@@ -1,3 +1,5 @@
+
+import org.apache.poi.hssf.util.HSSFColor;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.ss.usermodel.charts.*;
 import org.apache.poi.ss.util.CellRangeAddress;
@@ -11,54 +13,56 @@ import java.util.Map;
 
 public class XlsGenerator {
 
+	public Workbook wb = new XSSFWorkbook();
+
 	public XlsGenerator(String path, String[] columnsData, HashMap<String, Double[]> dataMap) {
-		try {
 
-			int line = 1;
-			int column = 1;
-			int margin = 2;
 
-			Workbook wb = new XSSFWorkbook();
-			Sheet sheet =  wb.createSheet("new sheet");
+		int line = 1;
+		int column = 1;
+		int margin = 2;
 
-			Drawing drawing = sheet.createDrawingPatriarch();
-			//position of the graphs
-			int top = column;
-			int left = dataMap.size() + line + 1 + margin;
-			int bottom = column + columnsData.length + 1;
-			int right = dataMap.size() + line + 1 + margin + 10 ;
+		Sheet sheet = wb.createSheet("new sheet");
 
-			ClientAnchor anchor = drawing.createAnchor(0, 0, 0, 0, top, left, bottom, right);
+		Drawing drawing = sheet.createDrawingPatriarch();
+		//position of the graphs
+		int top = column;
+		int left = dataMap.size() + line + 1 + margin;
+		int bottom = column + columnsData.length + 1;
+		int right = dataMap.size() + line + 1 + margin + 10;
 
-			Chart chart = drawing.createChart(anchor);
-			ChartLegend legend = chart.getOrCreateLegend();
-			legend.setPosition(LegendPosition.TOP_RIGHT);
+		ClientAnchor anchor = drawing.createAnchor(0, 0, 0, 0, top, left, bottom, right);
 
-			LineChartData data = chart.getChartDataFactory().createLineChartData();
+		Chart chart = drawing.createChart(anchor);
+		ChartLegend legend = chart.getOrCreateLegend();
+		legend.setPosition(LegendPosition.TOP_RIGHT);
 
-			ChartAxis bottomAxis = chart.getChartAxisFactory().createCategoryAxis(AxisPosition.BOTTOM);
-			ValueAxis leftAxis = chart.getChartAxisFactory().createValueAxis(AxisPosition.LEFT);
-			leftAxis.setCrosses(AxisCrosses.AUTO_ZERO);
+		LineChartData data = chart.getChartDataFactory().createLineChartData();
 
-			// Generate title
-			insertCellLine(sheet, line, column, "", columnsData);
-			ChartDataSource<String> xs = DataSources.fromStringCellRange(sheet, new CellRangeAddress(line, line, column + 1, columnsData.length + 1));
+		ChartAxis bottomAxis = chart.getChartAxisFactory().createCategoryAxis(AxisPosition.BOTTOM);
+		ValueAxis leftAxis = chart.getChartAxisFactory().createValueAxis(AxisPosition.LEFT);
+		leftAxis.setCrosses(AxisCrosses.AUTO_ZERO);
+
+		// Generate title
+		insertCellLine(sheet, line, column, "", columnsData);
+		ChartDataSource<String> xs = DataSources.fromStringCellRange(sheet, new CellRangeAddress(line, line, column + 1, columnsData.length + 1));
+		line++;
+
+		for (Map.Entry<String, Double[]> entry : dataMap.entrySet()) {
+			String key = entry.getKey();
+			Double[] value = entry.getValue();
+			insertCellLine(sheet, line, column, key, value);
+
+			// Generate Series
+			ChartDataSource<Number> y = DataSources.fromNumericCellRange(sheet, new CellRangeAddress(line, line, column + 1, value.length + 1));
+			LineChartSeries chartSerie = data.addSeries(xs, y);
+			chartSerie.setTitle(key);
 			line++;
+		}
 
-			for(Map.Entry<String, Double[]> entry : dataMap.entrySet()) {
-				String key = entry.getKey();
-				Double[] value = entry.getValue();
-				insertCellLine(sheet, line, column, key, value);
+		chart.plot(data, bottomAxis, leftAxis);
 
-				// Generate Series
-				ChartDataSource<Number> y = DataSources.fromNumericCellRange(sheet, new CellRangeAddress(line, line, column + 1, value.length + 1));
-				LineChartSeries chartSerie = data.addSeries(xs, y);
-				chartSerie.setTitle(key);
-				line++;
-			}
-
-			chart.plot(data, bottomAxis, leftAxis);
-
+		try {
 			FileOutputStream fileOut = new FileOutputStream(path);
 			wb.write(fileOut);
 			fileOut.close();
@@ -73,10 +77,12 @@ public class XlsGenerator {
 		int column = startColumn + 1;
 		Cell cell = row.createCell(startColumn);
 		cell.setCellValue(title);
+		cell.setCellStyle(getTitleCellStyle());
 
-		for (String colData: data) {
+		for (String colData : data) {
 			cell = row.createCell(column);
 			cell.setCellValue(title);
+			cell.setCellStyle(getTitleCellStyle());
 			cell.setCellValue(colData);
 			column++;
 		}
@@ -87,12 +93,32 @@ public class XlsGenerator {
 		int column = startColumn + 1;
 		Cell cell = row.createCell(startColumn);
 		cell.setCellValue(title);
+		cell.setCellStyle(getTitleCellStyle());
 
-		for (Double colData: data) {
+		for (Double colData : data) {
 			cell = row.createCell(column);
 			cell.setCellValue(title);
+			cell.setCellStyle(getCellStyle());
 			cell.setCellValue(colData);
 			column++;
 		}
+
+	}
+
+	private CellStyle getCellStyle() {
+		CellStyle cellStyle = wb.createCellStyle();
+		cellStyle.setBorderTop(BorderStyle.THIN);
+		cellStyle.setBorderBottom(BorderStyle.THIN);
+		cellStyle.setBorderLeft(BorderStyle.THIN);
+		cellStyle.setBorderRight(BorderStyle.THIN);
+		return cellStyle;
+
+	}
+
+	private CellStyle getTitleCellStyle(){
+		CellStyle cellStyle = getCellStyle();
+		cellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+		cellStyle.setFillForegroundColor(HSSFColor.GREY_25_PERCENT.index);
+		return cellStyle;
 	}
 }
